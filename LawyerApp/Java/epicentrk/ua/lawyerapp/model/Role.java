d85b58cb44fe49f94d723cece1d68f193904a417
Laschuk Oleksandr <Slash@rv.epicentrk.com>
2016-12-22 20:16:11 +0200

    Third commit

---
+++ LawyerApp/Java/epicentrk/ua/lawyerapp/model/Role.java
@@ -0,0 +1,12 @@
+package epicentrk.ua.lawyerapp.model;
+
+/*
+ * Simple JavaBean object that represents role of (@link User).
+ * @author Laschuk Oleksandr
+ * @version 1.0
+ */
+@Entity
+@Table
+public class Role {
+
+}
